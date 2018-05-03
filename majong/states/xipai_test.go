65cb4c94e13e71fb54a41bc539d45d0b970f44ab
安佳玮 <anjiawei@stevegame.cn>
2018-05-03 17:08:04 +0800

    洗牌和发牌

---
+++ majong/states/xipai_test.go
@@ -0,0 +1,34 @@
+package states
+
+import (
+	"fmt"
+	"steve/majong/interfaces"
+	majongpb "steve/server_pb/majong"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestXipaiState_xipai(t *testing.T) {
+	mc := gomock.NewController(t)
+	flow := interfaces.NewMockMajongFlow(mc)
+
+	flow.EXPECT().GetMajongContext().Return(
+		&majongpb.MajongContext{
+			GameId: 1,
+		},
+	).AnyTimes()
+
+	s := XipaiState{}
+
+	cards := s.xipai(flow)
+	results := ""
+	for _, card := range cards {
+		results = fmt.Sprintf("%s\n%v %v", results, card.GetColor(), card.GetPoint())
+	}
+	logrus.Info(results)
+	assert.NotEqual(t, 0, len(results))
+}
