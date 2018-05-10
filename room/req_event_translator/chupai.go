3dfb57a5675874b99f4f8e03b26dbbc23da09d57
wuhongwei <wuhongwei@stevegame.cn>
2018-05-10 11:08:32 +0800

    添加工厂创建状态，明杠，暗杠，补杠，碰，定缺状态，和结算相关方法

---
+++ room/req_event_translator/chupai.go
@@ -0,0 +1,23 @@
+package rtoet
+
+import (
+	"steve/client_pb/room"
+	server_pb "steve/server_pb/majong"
+	"steve/structs/proto/gate_rpc"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func translateChupaiReq(playerID uint64, header *steve_proto_gaterpc.Header,
+	req room.RoomChupaiReq) (eventID server_pb.EventID, eventContext proto.Message, err error) {
+
+	eventHeader := translateHeader(playerID, header, &req)
+
+	card := translateCard(*req.GetCard())
+	eventContext = &server_pb.ChupaiRequestEvent{
+		Head:  &eventHeader,
+		Cards: &card,
+	}
+	eventID = server_pb.EventID_event_chupai_request
+	return
+}
