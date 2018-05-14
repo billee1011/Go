afec9a9c328964b6d03cecc7f2f2e4183d734b1d
安佳玮 <anjiawei@stevegame.cn>
2018-05-14 20:11:53 +0800

    修改协议，对应修改代码

--- room/req_event_translator/req_event_translator.go
+++ room/req_event_translator/req_event_translator.go
@@ -111,8 +111,7 @@ func (t *translator) addTranslator(msgID msgid.MsgID, f interface{}) {
 func (t *translator) addTranslators() {
 	// TODO 添加所有请求转事件表
 	t.addTranslator(msgid.MsgID_ROOM_HUANSANZHANG_REQ, translateHuansanzhangReq)
-	t.addTranslator(msgid.MsgID_ROOM_HU_REQ, translateHuRequest)
-	t.addTranslator(msgid.MsgID_ROOM_GANG_REQ, translateGangReq)
+	t.addTranslator(msgid.MsgID_ROOM_XINGPAI_ACTION_REQ, translateXingpaiActionReq)
 	t.addTranslator(msgid.MsgID_ROOM_CHUPAI_REQ, translateChupaiReq)
 	t.addTranslator(msgid.MsgID_ROOM_DINGQUE_REQ, translateDingqueReq)
 }
---
+++ room/req_event_translator/xingpai_action.go
@@ -0,0 +1,53 @@
+package rtoet
+
+import (
+	"errors"
+	"steve/client_pb/room"
+	server_pb "steve/server_pb/majong"
+	"steve/structs/proto/gate_rpc"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func translateXingpaiActionReq(playerID uint64, header *steve_proto_gaterpc.Header,
+	req room.RoomXingpaiActionReq) (eventID server_pb.EventID, eventContext proto.Message, err error) {
+
+	eventHeader := translateHeader(playerID, header, &req)
+	switch req.GetActionId() {
+	case room.XingpaiAction_XA_CHI:
+		{
+			err = errors.New("未实现")
+		}
+	case room.XingpaiAction_XA_PENG:
+		{
+			eventID = server_pb.EventID_event_peng_request
+			eventContext = &server_pb.PengRequestEvent{
+				Head: &eventHeader,
+			}
+		}
+	case room.XingpaiAction_XA_GANG:
+		{
+			eventID = server_pb.EventID_event_gang_request
+			card := translateCard(*req.GetGangCard())
+			eventContext = &server_pb.GangRequestEvent{
+				Head: &eventHeader,
+				Card: &card,
+			}
+		}
+	case room.XingpaiAction_XA_HU:
+		{
+			eventID = server_pb.EventID_event_hu_request
+			eventContext = &server_pb.HuRequestEvent{
+				Head: &eventHeader,
+			}
+		}
+	case room.XingpaiAction_XA_QI:
+		{
+			eventID = server_pb.EventID_event_qi_request
+			eventContext = &server_pb.QiRequestEvent{
+				Head: &eventHeader,
+			}
+		}
+	}
+	return
+}
