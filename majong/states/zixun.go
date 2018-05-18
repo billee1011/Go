92df19fa17b7a912d2bd0ff1ced731e3f10e97ae
安佳玮 <anjiawei@stevegame.cn>
2018-05-18 02:42:05 +0000

    Update zixun.go

--- majong/states/zixun.go
+++ majong/states/zixun.go
@@ -371,10 +371,6 @@ func (s *ZiXunState) checkBuGang(context *majongpb.MajongContext) (bool, []uint3
 	//分两种情况查暗杠，一种是胡牌前，一种胡牌后
 	hasHu := len(activePlayer.GetHuCards()) > 0
 	pengCards := activePlayer.GetPengCards()
-	logrus.WithFields(logrus.Fields{
-		"player":    activePlayerID,
-		"pengCards": pengCards,
-	}).Info("碰过的牌")
 	enableBugangCards := make([]uint32, 0, 0)
 	// actioninfos := []*clientpb.ActionInfo{}
 	for _, touchCard := range activePlayer.HandCards {
