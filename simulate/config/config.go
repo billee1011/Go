1d670cb86a6195c56e539e2b7c9ae7cdc39320f5
pizhiwen <pizhiwen@stevegame.cn>
2018-05-19 14:38:50 +0800

    还原配置为默认

--- simulate/config/config.go
+++ simulate/config/config.go
@@ -2,10 +2,10 @@ package config
 
 const (
 	// ServerAddr 服务器地址
-	ServerAddr = "127.0.0.1:36003"
+	ServerAddr = "127.0.0.1:36001"
 	// ClientVersion 客户端版本号
 	ClientVersion = "1.0"
 
 	// PeipaiURL 配牌服务地址
-	PeipaiURL = "http://127.0.0.1:36004"
+	PeipaiURL = "http://127.0.0.1:36002"
 )
