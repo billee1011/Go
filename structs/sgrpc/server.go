f767a9e38e54ce39fb2d9163bff7dc3288d90eb2
安佳玮 <anjiawei@stevegame.cn>
2018-05-30 18:21:33 +0800

    提供负载上报服务注册接口

--- structs/sgrpc/server.go
+++ structs/sgrpc/server.go
@@ -1,8 +1,18 @@
 package sgrpc
 
+import (
+	grpclb "github.com/bsm/grpclb/grpclb_backend_v1"
+)
+
 // RPCServer RPC服务
 type RPCServer interface {
 	// RegisterService 使用protoc生成的函数注册服务
 	// Example: RegisterService(RegisterRsctlServiceServer, someimpl)
 	RegisterService(f interface{}, service interface{}) error
+}
+
+// RegisterLoadReporterService 注册负载上报服务
+// 服务可以使用 grpclb/reporter 下面的通用 Reporter 或者使用自己实现的 Reporter 来注册
+func RegisterLoadReporterService(lps grpclb.LoadReportServer, s RPCServer) error {
+	return s.RegisterService(grpclb.RegisterLoadReportServer, lps)
 }
