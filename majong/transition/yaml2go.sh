a28850972fa7b17bbc6659b2465f9553d06558f6
郑旭章 <zhengxuzhang@stevegame.cn>
2018-06-14 19:11:39 +0800

    修改yaml生成sh脚本

--- majong/transition/yaml2go.sh
+++ majong/transition/yaml2go.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 #将当前目录下的yaml文件转换成go文件
+gofileName="transition_cfg.go"
+rm ${gofileName}
+touch ${gofileName}
+echo "package transition" >> ${gofileName}
+SSID='`'
+echo "var transitionCfg = ${SSID}" >> ${gofileName}
 for file in ./*
 do
     if test -f $file -a "${file##*.}"x = "yaml"x #遍历当前目录下所有yaml文件
     then
-        basefileName=`basename $file  .yaml` 
-        gofileName=${basefileName}"_cfg.go"
-        rm ${gofileName}
-        touch ${gofileName}
-        echo "package ${basefileName}" >> ${gofileName}
-        SSID='`'
-        echo "var ${basefileName}Cfg = ${SSID}" >> ${gofileName}
         IFS_old=$IFS
         while read line;
         do
@@ -18,6 +17,6 @@ do
             echo "$line" >> ${gofileName}
         done < $file
         IFS=$IFS_old
-        echo "${SSID}" >> ${gofileName}
     fi
 done
+echo "${SSID}" >> ${gofileName}
