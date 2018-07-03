e683c60f6751b4575e253bf253229b2f157aef79
李全林 <liquanlin@stevegame.cn>
2018-07-03 17:55:35 +0800

    斗地主开局和发牌

--- room/desks/ddzdesk/flow/ddz/states/init.go
+++ room/desks/ddzdesk/flow/ddz/states/init.go
@@ -5,6 +5,9 @@ import (
 	"steve/server_pb/ddz"
 
 	"github.com/Sirupsen/logrus"
+	"steve/client_pb/msgId"
+	"steve/client_pb/room"
+	"math/rand"
 )
 
 type initState struct{}
@@ -24,7 +27,16 @@ func (s *initState) OnEvent(m machine.Machine, event machine.Event) (int, error)
 	return int(ddz.StateID_state_init), nil
 }
 
+// 开局随即确定一个叫地主玩家,然后广播通知游戏开始
 func (s *initState) onStartGame(m machine.Machine) (int, error) {
 	logrus.WithField("context", getDDZContext(m)).Debugln("开始游戏")
+	players := getPlayers(m)
+	i := rand.Intn(len(players))
+	callPlayer := players[i+1]//叫地主玩家
+	var stageTime uint32 = 4;
+	broadcast(m, msgid.MsgID_ROOM_DDZ_START_GAME_NTF, &room.DDZStartGameNtf{
+		PlayerId:&callPlayer,
+		NextStage:&room.NextStage{Stage: room.DDZStage_DDZ_STAGE_DEAL.Enum(), Time: &stageTime},
+	})
 	return int(ddz.StateID_state_deal), nil
 }
--- room/desks/ddzdesk/flow/ddz/states/utils.go
+++ room/desks/ddzdesk/flow/ddz/states/utils.go
@@ -20,11 +20,56 @@ func getDDZContext(m machine.Machine) *ddz.DDZContext {
 	return dm.GetDDZContext()
 }
 
+func getPlayers(m machine.Machine) []uint64 {
+	dm, ok := m.(*ddzmachine.DDZMachine)
+	if !ok {
+		return nil
+	}
+
+	players := []uint64{}
+
+	for _, player := range dm.GetDDZContext().GetPlayers() {
+		players = append(players, player.GetPalyerId())
+	}
+	return players
+}
+
 // sendMessage 向玩家发送消息
 func sendMessage(m machine.Machine, players []uint64, msgID msgid.MsgID, body proto.Message) error {
 	dm, ok := m.(*ddzmachine.DDZMachine)
 	if !ok {
 		return fmt.Errorf("不是斗地主状态机")
+	}
+	return dm.SendMessage(players, msgID, body)
+}
+
+func sendToPlayer(m machine.Machine, playerID uint64, msgID msgid.MsgID, body proto.Message) error {
+	dm, ok := m.(*ddzmachine.DDZMachine)
+	if !ok {
+		return fmt.Errorf("不是斗地主状态机")
+	}
+	return dm.SendMessage([]uint64{playerID}, msgID, body)
+}
+
+func broadcast(m machine.Machine, msgID msgid.MsgID, body proto.Message) error {
+	dm, ok := m.(*ddzmachine.DDZMachine)
+	if !ok {
+		return fmt.Errorf("不是斗地主状态机")
+	}
+	return dm.SendMessage(getPlayers(m), msgID, body)
+}
+
+func broadcastExcept(m machine.Machine, playerID uint64, msgID msgid.MsgID, body proto.Message) error {
+	dm, ok := m.(*ddzmachine.DDZMachine)
+	if !ok {
+		return fmt.Errorf("不是斗地主状态机")
+	}
+	allPlayers := getPlayers(m)
+	players := []uint64{}
+	for _, pid := range allPlayers {
+		if pid != playerID {
+			players = append(players, pid)
+		}
 	}
 	return dm.SendMessage(players, msgID, body)
 }
