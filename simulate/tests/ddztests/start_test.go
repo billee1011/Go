4e2c40825116f2b38163c3ef060d6d8e5ae6fbf3
杨兆丰 <yangzhaofeng@stevegame.cn>
2018-07-12 15:20:30 +0800

    斗地主测试代码

--- simulate/tests/ddztests/start_test.go
+++ simulate/tests/ddztests/start_test.go
@@ -13,15 +13,8 @@ func Test_StartGame(t *testing.T) {
 	// 扑克游戏的参数
 	params := global.NewStartDDZGameParams()
 
-	// 开始扑克游戏
-	//ddzGame, err := game.StartGame(params)
-
-	//assert.NotNil(t, ddzGame)
-	//assert.Nil(t, err)
-
 	deskData, err := utils.StartDDZGame(params)
-	//fmt.Println("%v", err)
-	//deskData, err := utils.StartDDZGame(params)
+
 	assert.NotNil(t, deskData)
 	assert.Nil(t, err)
 }
