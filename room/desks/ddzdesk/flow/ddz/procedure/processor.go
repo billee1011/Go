afbd84a403208d084c11f78b223b1955ad31cf4c
杨兆丰 <yangzhaofeng@stevegame.cn>
2018-07-16 20:25:32 +0800

    斗地主恢复对局修改

--- room/desks/ddzdesk/flow/ddz/procedure/processor.go
+++ room/desks/ddzdesk/flow/ddz/procedure/processor.go
@@ -52,8 +52,8 @@ func HandleEvent(params Params) (result Result) {
 	if params.EventID == int(ddz.EventID_event_resume_request) {
 		if dealResumeRequest(params.EventContext, m, &cloneContext) == false {
 			logEntry.Errorln("处理恢复对局失败")
-			return
 		}
+		return
 	}
 
 	err := m.ProcessEvent(machine.Event{
@@ -134,7 +134,98 @@ func dealResumeRequest(eventContext []byte, machine *ddzmachine.DDZMachine, ddzC
 			ddzPlayerInfo.Lord = &lord
 			ddzPlayerInfo.Tuoguan = &tuoguan
 			//ddzPlayerInfo.Grablord =
-			//ddzPlayerInfo.DoubleType
+
+			// 叫/抢地主
+			if ddzContext.CurState == ddz.StateID_state_grab {
+
+				grabLord := room.GrabLordType_GLT_CALLLORD      // 叫地主
+				notGrabLord := room.GrabLordType_GLT_NOTCALLORD // 不叫
+				grab := room.GrabLordType_GLT_GRAB              // 抢地主
+				notGrab := room.GrabLordType_GLT_NOTGRAB        // 不抢
+				noneOpe := room.GrabLordType_GLT_NONE           // 未操作
+
+				// 首个叫地主玩家的playerID
+				firstPlayerID := ddzContext.GetFirstGrabPlayerId()
+
+				// 自己是否操作过
+				bOpera := false
+				allOpePlayers := ddzContext.GetGrabbedPlayers()
+				for i := 0; i < len(allOpePlayers); i++ {
+					if allOpePlayers[i] == player.GetPlayerId() {
+						bOpera = true
+						break
+					}
+				}
+
+				// 没人叫过地主
+				if firstPlayerID == 0 {
+					if player.GetGrab() {
+						logEntry.Error("出现错误！firstPlayerID为0，但是player.GetGrab()为true")
+						ddzPlayerInfo.GrabLord = &grabLord
+					} else {
+						// 操作过说明不叫，否则说明未操作
+						if bOpera {
+							ddzPlayerInfo.GrabLord = &notGrabLord
+						} else {
+							ddzPlayerInfo.GrabLord = &noneOpe
+						}
+					}
+				} else { // 已经有人叫过地主了
+
+					// 自己叫/抢过
+					if player.GetGrab() {
+
+						// 首次的是自己，说明是叫地主; 首次的不是自己，说明是抢地主
+						if ddzContext.GetFirstGrabPlayerId() == player.GetPlayerId() {
+							ddzPlayerInfo.GrabLord = &grabLord
+						} else {
+							ddzPlayerInfo.GrabLord = &grab
+						}
+					} else { // 自己没叫/抢过
+						// 操作过说明是不叫/不抢，否则说明未操作
+						if bExist {
+							// 指定叫地主的是自己，说明是不叫;否则说明是不抢
+							if ddzContext.GetCallPlayerId() == player.GetPlayerId() {
+								ddzPlayerInfo.GrabLord = &notGrabLord
+							} else {
+								ddzPlayerInfo.GrabLord = &notGrab
+							}
+						} else {
+							ddzPlayerInfo.GrabLord = &noneOpe
+						}
+					}
+				}
+			}
+
+			// 加倍状态
+			if ddzContext.CurState == ddz.StateID_state_double {
+
+				double := room.DoubleType_DT_DOUBLE
+				notDouble := room.DoubleType_DT_NOTDOUBLE
+				noneOpera := room.DoubleType_DT_NONE
+
+				// 加倍
+				if player.GetIsDouble() {
+					ddzPlayerInfo.Double = &double
+				} else {
+					// 是否已操作过
+					bExist := false
+					allOpePlayers := ddzContext.GetDoubledPlayers()
+					for i := 0; i < len(allOpePlayers); i++ {
+						if allOpePlayers[i] == player.GetPlayerId() {
+							bExist = true
+							break
+						}
+					}
+
+					// 存在说明不加倍，不存在说明未操作
+					if bExist {
+						ddzPlayerInfo.Double = &notDouble
+					} else {
+						ddzPlayerInfo.Double = &noneOpera
+					}
+				}
+			}
 
 			playersInfo = append(playersInfo, &ddzPlayerInfo)
 		}
@@ -150,7 +241,11 @@ func dealResumeRequest(eventContext []byte, machine *ddzmachine.DDZMachine, ddzC
 		duration := time.Second * time.Duration(ddzContext.Duration)
 
 		// 剩余时间
-		leftTime := uint32(duration - time.Now().Sub(startTime))
+		leftTime := duration - time.Now().Sub(startTime)
+
+		if leftTime < 0 {
+			leftTime = 0
+		}
 
 		// 发送游戏信息
 		machine.SendMessage([]uint64{reqPlayerID}, msgid.MsgID_ROOM_DDZ_RESUME_RSP, &room.DDZResumeGameRsp{
@@ -158,8 +253,8 @@ func dealResumeRequest(eventContext []byte, machine *ddzmachine.DDZMachine, ddzC
 			GameInfo: &room.DDZDeskInfo{
 				Players:           playersInfo,                                                    // 每个人的信息
 				Stage:             states.GenNextStage(room.DDZStage(int32(ddzContext.CurStage))), // 当前状态
-				CurPlayerID:       proto.Uint64(ddzContext.GetCurrentPlayerId()),                  // 当前操作的玩家
-				CurPlayerLeftTime: proto.Uint32(leftTime),                                         // 剩余操作的时间
+				CurPlayerId:       proto.Uint64(ddzContext.GetCurrentPlayerId()),                  // 当前操作的玩家
+				CurPlayerLeftTime: proto.Uint32(uint32(leftTime)),                                 // 剩余操作的时间
 			},
 		})
 	}
