32afb17044d44fa58f2fbba49fcabaeb901ebdf5
fujibayashikyo <286shei@163.com>
2018-07-19 09:38:04 +0800

    第一次提交

--- room2/util/util.go
+++ room2/util/util.go
@@ -19,6 +19,21 @@ func TranslateToRoomPlayer(player *player.Player) room.RoomPlayerInfo {
 	}
 }
 
+// GetHszFx 根据配牌信息获取换三张方向
+func GetHszFx(fxValue string) (fx int) {
+	switch fxValue {
+	case "dui":
+		fx = 1
+	case "shun":
+		fx = 0
+	case "ni":
+		fx = 2
+	default:
+		fx = -1
+	}
+	return
+}
+
 
 // CanTingCardInfoSvr2Client 玩家停牌信息转换，server_pb-->client_pb
 func CanTingCardInfoSvr2Client(minfos []*majongpb.CanTingCardInfo) []*room.CanTingCardInfo {
