673da454bfb08c6ae09d0eaa3053a474ca424d15
SkyWang <569258817@qq.com>
2018-07-20 14:33:46 +0800

    1.支持默认结算器。默认结算器=四川血流结算器。

--- majong/settle/factory.go
+++ majong/settle/factory.go
@@ -9,13 +9,12 @@ package settle
 
 import (
 	"steve/majong/interfaces"
-	"steve/majong/settle/null"
 	"steve/majong/settle/majong"
 )
 // map[游戏Id]*结算管理器
 // 不同子游戏定义不同的结算管理器
 var mapSettle =  map[int32] *settlerMgr{
-	0 : {gangSettle: &null.GangSettle{}, huSettle: &null.HuSettle{}, roundSettle: &null.RoundSettle{}},
+	0 : {gangSettle: &majong.GangSettle{}, huSettle: &majong.HuSettle{}, roundSettle: &majong.RoundSettle{}},
 	1 : {gangSettle: &majong.GangSettle{}, huSettle: &majong.HuSettle{}, roundSettle: &majong.RoundSettle{}},
 	2: {gangSettle: &majong.GangSettle{}, huSettle: &majong.HuSettle{}, roundSettle: &majong.RoundSettle{}},
 }
@@ -38,15 +37,26 @@ type SettlerFactory struct{}
 
 // CreateGangSettler 创建杠结算器
 func (f *SettlerFactory) CreateGangSettler(gameId int32) interfaces.GangSettle {
-	return mapSettle[gameId].gangSettle
+	if t, ok := mapSettle[gameId]; ok {
+		return t.gangSettle
+	}
+	return mapSettle[0].gangSettle
 }
 
 // CreateHuSettler 创建胡结算器
 func (f *SettlerFactory) CreateHuSettler(gameId int32) interfaces.HuSettle {
-	return mapSettle[gameId].huSettle
+	if t, ok := mapSettle[gameId]; ok {
+		return t.huSettle
+	}
+	return mapSettle[0].huSettle
+
 }
 
 // CreateRoundSettle 创建单局结算器
 func (f *SettlerFactory) CreateRoundSettle(gameId int32) interfaces.RoundSettle {
-	return mapSettle[gameId].roundSettle
+	if t, ok := mapSettle[gameId]; ok {
+		return t.roundSettle
+	}
+	return mapSettle[0].roundSettle
+
 }
