42fd2e094154f10af0ff580df13e234298b3e68e
chenhuahua <chenhuahua@stevegame.cn>
2018-07-24 12:30:00 +0800

    fix roomv2 cycle dependency

--- room2/util/game_mj_config.go
+++ room2/util/game_mj_config.go
@@ -1,32 +1,15 @@
 package util
 
-import "github.com/spf13/viper"
-
-const (
-	// ListenClientAddr 代表监听客户端的IP地址，默认值为 127.0.0.1
-	ListenClientAddr = "lis_client_addr"
-
-	// ListenClientPort 代表监听客户端的端口， 默认值为 36001
-	ListenClientPort = "lis_client_port"
-
-	// ListenPeipaiAddr 配牌監聽地址
-	ListenPeipaiAddr = "peipai_addr"
-
-	// XingPaiTimeOut 行牌超时时间，单位为second，默认值为 10
-	XingPaiTimeOut = "xp_timeout"
-
-	// MaxFapaiCartoonTime 发牌的动画时间
-	MaxFapaiCartoonTime = "fp_cartoontime"
-
-	// MaxHuansanzhangCartoonTime 换三张动画时间
-	MaxHuansanzhangCartoonTime = "hsz_cartoontime"
+import (
+	"github.com/spf13/viper"
+	"steve/room2/fixed"
 )
 
 func init() {
-	viper.SetDefault(ListenClientAddr, "127.0.0.1")
-	viper.SetDefault(ListenClientPort, 36001)
-	viper.SetDefault(ListenPeipaiAddr, "")
-	viper.SetDefault(XingPaiTimeOut, 10)
-	viper.SetDefault(MaxFapaiCartoonTime, 10*1000)
-	viper.SetDefault(MaxHuansanzhangCartoonTime, 10*1000)
+	viper.SetDefault(fixed.ListenClientAddr, "127.0.0.1")
+	viper.SetDefault(fixed.ListenClientPort, 36001)
+	viper.SetDefault(fixed.ListenPeipaiAddr, "")
+	viper.SetDefault(fixed.XingPaiTimeOut, 10)
+	viper.SetDefault(fixed.MaxFapaiCartoonTime, 10*1000)
+	viper.SetDefault(fixed.MaxHuansanzhangCartoonTime, 10*1000)
 }
