91d78f86f697d1f1ff02627536c1da564d132b8b
chenhuahua <chenhuahua@stevegame.cn>
2018-07-26 16:17:56 +0800

    add watch example

---
+++ serviceexample/redisWatchExample/watch.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	var incr func(string) error
+
+	incr = func(key string) error {
+		err := client.Watch(func(tx *redis.Tx) error {
+			n, err := tx.Get(key).Int64()
+			if err != nil && err != redis.Nil {
+				return err
+			}
+
+			_, err = tx.Pipelined(func(pipe redis.Pipeliner) error {
+				pipe.Set(key, strconv.FormatInt(n+1, 10), 0)
+				return nil
+			})
+			return err
+		}, key)
+		if err == redis.TxFailedErr {
+			return incr(key)
+		}
+		return err
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			err := incr("counter3")
+			if err != nil {
+				panic(err)
+			}
+		}()
+	}
+	wg.Wait()
+
+	n, err := client.Get("counter3").Int64()
+	fmt.Println(n, err)
+}
