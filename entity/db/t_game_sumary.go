7e2621beb056a721d05e7d8f684e7df5d65a48cc
chenhuahua <chenhuahua@stevegame.cn>
2018-08-01 17:04:39 +0800

    update mysql table model

--- entity/db/t_game_sumary.go
+++ entity/db/t_game_sumary.go
@@ -12,9 +12,9 @@ type TGameSumary struct {
 	Playerids     string    `xorm:"comment('桌子内玩家，多个玩家用|分割') VARCHAR(256)"`
 	Scoreinfo     string    `xorm:"comment('输赢分,顺序和ID相同') VARCHAR(256)"`
 	Winnerids     string    `xorm:"comment('赢家ID，多个赢家用|分割') VARCHAR(256)"`
+	Roundcurrency string    `xorm:"TEXT"`
 	Createtime    time.Time `xorm:"DATETIME"`
 	Createby      string    `xorm:"VARCHAR(64)"`
 	Updatetime    time.Time `xorm:"DATETIME"`
 	Updateby      string    `xorm:"VARCHAR(64)"`
-	Roundcurrency string    `xorm:"TEXT"`
 }
