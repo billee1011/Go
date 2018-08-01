71cd84c55c632f3e71c003c4c2e00c352c990c79
SkyWang <569258817@qq.com>
2018-08-01 11:52:45 +0800

    1.优化编译脚本。

--- gold/build.sh
+++ gold/build.sh
@@ -4,10 +4,23 @@
 NAME=gold
 
 mkdir ../bin/$NAME
+echo "begin building..."
 go build  -o ./bin/$NAME.so -buildmode=plugin steve/$NAME
-cp ./config.yml ../bin/$NAME/
-cp ./config.yml  ./bin/$NAME/
-cp ./bin/$NAME.so ../bin/$NAME/
+
+echo "begin copy yml"
+cp  -f ./config.yml ../bin/$NAME/
+cp -f  ./config.yml  ./bin/
+
+echo "begin copy start.sh, stop.sh"
+cp -f ./start.sh ../bin/$NAME/
+cp -f  ./start.sh  ./bin/
+cp -f ./stop.sh ../bin/$NAME/
+cp -f  ./stop.sh  ./bin/
+
+echo "begin cp so"
+cp -f  ./bin/$NAME.so ../bin/$NAME/
+
+echo "end  build..."
 
 
 
