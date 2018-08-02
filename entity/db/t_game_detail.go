093bde57678b6144406ee3e87d758440897476dc
张世虎 <zhangshihu@stevesoft.cn>
2018-08-02 17:24:48 +0800

    数据库表game_detail的deskid字段修改

--- entity/db/t_game_detail.go
+++ entity/db/t_game_detail.go
@@ -7,10 +7,10 @@ import (
 type TGameDetail struct {
 	Sumaryid   int64     `xorm:"not null pk BIGINT(20)"`
 	Playerid   int64     `xorm:"not null BIGINT(20)"`
-	Deskid     int64     `xorm:" BIGINT(20)"`
+	Deskid     int64     `xorm:"BIGINT(20)"`
 	Gameid     int       `xorm:"INT(11)"`
 	Amount     int64     `xorm:"BIGINT(20)"`
-	Iswinner   bool      `xorm:"TINYINT(1)"`
+	Iswinner   int       `xorm:"TINYINT(1)"`
 	Createtime time.Time `xorm:"DATETIME"`
 	Createby   string    `xorm:"VARCHAR(64)"`
 	Updatetime time.Time `xorm:"DATETIME"`
