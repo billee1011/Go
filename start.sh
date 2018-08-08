1c77aef3e622cd5ecbae644b16f2e5a53a80dcbe
李全林 <liquanlin@stevegame.cn>
2018-08-08 09:38:44 +0800

    启动脚本不要用nohup启动，需要查看console日志

--- start.sh
+++ start.sh
@@ -8,37 +8,37 @@ popd
 sleep 2
 
 pushd gateway 
-nohup serviceloader gateway --config=config.yml  &
+serviceloader gateway --config=config.yml  &
 popd 
 
 pushd room 
-nohup serviceloader room --config=config.yml  &
+serviceloader room --config=config.yml  &
 popd 
 
 
 pushd hall 
-nohup serviceloader hall --config=config.yml  &
+serviceloader hall --config=config.yml  &
 popd 
 
 pushd login 
-nohup  serviceloader login --config=config.yml  &
+serviceloader login --config=config.yml  &
 popd 
 
 pushd match 
-nohup  serviceloader match --config=config.yml  &
+serviceloader match --config=config.yml  &
 popd 
 
 pushd robot 
-nohup serviceloader robot --config=config.yml  &
+serviceloader robot --config=config.yml  &
 popd
 
 pushd gold
-nohup serviceloader gold --config=config.yml  &
+serviceloader gold --config=config.yml  &
 popd
 
 
 pushd msgserver
-nohup serviceloader msgserver --config=config.yml  &
+serviceloader msgserver --config=config.yml  &
 popd
 
 
