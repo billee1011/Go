8cf754ad384a32f2f00658c4a902899fc2bd80b4
张世虎 <zhangshihu@stevesoft.cn>
2018-08-09 19:19:01 +0800

    玩家请求使用道具

--- common/data/prop/propconfig.go
+++ common/data/prop/propconfig.go
@@ -45,3 +45,26 @@ func GetSomePropsConfig(propIDs []int32) (propConfig []constant.PropAttr, err er
 
 	return
 }
+
+// GetOnePropsConfig 获取某些道具配置信息
+func GetOnePropsConfig(propID int32) (propConfig constant.PropAttr, err error) {
+	// 现在直接从数据库获取，后面改为先从redis获取；订阅更新消息，更新时删掉redis数据 TODO
+	val, err := configclient.GetConfig(constant.PropKey, constant.PropSubKey)
+	if err != nil {
+		return
+	}
+
+	var allConfig []constant.PropAttr
+	err = json.Unmarshal([]byte(val), allConfig)
+	if err != nil {
+		return
+	}
+
+	for _, config := range allConfig {
+		if propID == config.PropID {
+			propConfig = config
+		}
+	}
+
+	return
+}
--- room/register/handle.go
+++ room/register/handle.go
@@ -3,12 +3,14 @@ package registers
 import (
 	"steve/client_pb/msgid"
 	"steve/client_pb/room"
-	"steve/structs/exchanger"
-	"steve/structs/proto/gate_rpc"
-
+	playerdata "steve/common/data/player"
+	propclient "steve/common/data/prop"
+	"steve/gutils"
 	modelmanager "steve/room/models"
 	player2 "steve/room/player"
 	"steve/room/util"
+	"steve/structs/exchanger"
+	"steve/structs/proto/gate_rpc"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/golang/protobuf/proto"
@@ -117,4 +119,76 @@ func HandleTuoGuanReq(playerID uint64, header *steve_proto_gaterpc.Header, req r
 	}
 	player.SetTuoguan(req.GetTuoguan(), true)
 	return
+}
+
+// HandleUsePropReq 使用道具请求处理
+func HandleUsePropReq(playerID uint64, header *steve_proto_gaterpc.Header, req room.RoomUsePropReq) (ret []exchanger.ResponseMsg) {
+	logEntry := logrus.WithFields(logrus.Fields{
+		"func_name": "HandleUsePropReq",
+		"player_id": playerID,
+	})
+
+	errDesc := "不能使用该道具"
+	rsp := room.RoomUsePropRsp{
+		ErrCode: room.RoomError_FAILED.Enum(),
+		ErrDesc: &errDesc,
+	}
+	ret = []exchanger.ResponseMsg{
+		exchanger.ResponseMsg{
+			MsgID: uint32(msgid.MsgID_ROOM_USE_PROP_RSP),
+			Body:  &rsp,
+		},
+	}
+
+	player := player2.GetPlayerMgr().GetPlayer(playerID)
+	if player == nil {
+		logEntry.Debugln("获取玩家失败")
+		return
+	}
+	desk := player.GetDesk()
+	if desk == nil {
+		logEntry.Debugln("玩家不在房间中")
+		return
+	}
+
+	propID := gutils.PropTypeClient2Server(req.GetPropId())
+	prop, err := propclient.GetPlayerOneProp(playerID, propID)
+	if err != nil {
+		return
+	}
+
+	// 使用道具
+	if prop.Count > 0 {
+		err = propclient.AddPlayerProp(playerID, propID, -1)
+		if err != nil {
+			return
+		}
+	} else { // 使用金币购买
+		propConfig, err := propclient.GetOnePropsConfig(propID)
+		if err != nil {
+			return
+		}
+
+		coin := playerdata.GetPlayerCoin(playerID)
+		if coin > propConfig.Limit {
+			playerdata.AddPlayerCoin(playerID, propConfig.Value, desk.GetGameId(), desk.GetLevel())
+		} else {
+			return
+		}
+	}
+
+	// 广播道具
+	desPlayerID := req.GetPlayerId()
+	ntf := room.RoomUsePropNtf{
+		FromPlayerId: &playerID,
+		ToPlayerId:   &desPlayerID,
+		PropId:       req.PropId,
+	}
+	msgBody, err := proto.Marshal(&ntf)
+	if err != nil {
+		logEntry.WithError(err).Debugln("序列化失败")
+		return
+	}
+	modelmanager.GetModelManager().GetMessageModel(playerID).BroadcastMessage([]uint64{}, msgid.MsgID_ROOM_USE_PROP_NTF, msgBody, true)
+	return nil
 }
