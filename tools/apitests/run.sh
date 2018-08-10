0983d9927ab00b8ba96e6bf20004e85b074f8c06
安佳玮 <anjiawei@stevegame.cn>
2018-08-10 15:24:50 +0800

    增加 aptests 工具

---
+++ tools/apitests/run.sh
@@ -0,0 +1,2 @@
+./apitests login --productid=1 --url http://192.168.7.26:8086/mock/24/account/loginv2 --logintype 0 --channel 0 --username anjw --passwd abc --proid 4001 --cityid 4001 --wxappid 123344 --wxopenid 165654 --wxunionid 213231
+
