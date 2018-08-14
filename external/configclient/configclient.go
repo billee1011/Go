2affb66a7b43970469c44ab94dddf189c258b948
安佳玮 <anjiawei@stevegame.cn>
2018-08-14 16:25:08 +0800

    配置获取提供加载配置直到成功接口

--- external/configclient/configclient.go
+++ external/configclient/configclient.go
@@ -8,6 +8,7 @@ import (
 	entityConf "steve/entity/config"
 	"steve/server_pb/config"
 	"steve/structs"
+	"time"
 
 	"github.com/Sirupsen/logrus"
 )
@@ -35,6 +36,48 @@ func GetConfig(key, subkey string) (string, error) {
 	return val, nil
 }
 
+// GetConfigUntilSucc 获取配置直到成功
+func GetConfigUntilSucc(key, subkey string, maxRetry int, retryInterval time.Duration) (string, error) {
+	entry := logrus.WithFields(logrus.Fields{
+		"key":            key,
+		"subkey":         subkey,
+		"max_retry":      maxRetry,
+		"retry_interval": retryInterval,
+	})
+
+	var configCli config.ConfigClient
+	var err error
+	curRetry := 0
+	for {
+		configCli, err = getConfigCli()
+		if err == nil && configCli != nil {
+			break
+		}
+		curRetry++
+		if curRetry < maxRetry {
+			entry.Infoln("获取配置等待重试")
+			time.Sleep(retryInterval)
+			continue
+		}
+		break
+	}
+	if err != nil || configCli == nil {
+		return "", err
+	}
+	rsp, err := configCli.GetConfig(context.Background(), &config.GetConfigReq{
+		Key: key, Subkey: subkey,
+	})
+	if err != nil {
+		return "", fmt.Errorf("rpc 调用失败:%v", err.Error())
+	}
+	val := rsp.GetValue()
+	errCode := rsp.GetErrCode()
+	if errCode != 0 {
+		return "", fmt.Errorf("获取配置失败，错误码：%d", errCode)
+	}
+	return val, nil
+}
+
 func ParseToGameLevelConfigMap(jsonStr string) (conf []entityConf.GameLevelConfig) {
 	if err := json.Unmarshal([]byte(jsonStr), &conf); err != nil {
 		logrus.Errorf("游戏配置数据反序列化失败：%s", err.Error())
@@ -43,7 +86,7 @@ func ParseToGameLevelConfigMap(jsonStr string) (conf []entityConf.GameLevelConfi
 }
 
 //获取救济金配置
-func GetAlmsConfigMap() (conf []entityConf.AlmsConfig,err error){
+func GetAlmsConfigMap() (conf []entityConf.AlmsConfig, err error) {
 	almsStr, err := GetConfig("game", "alms")
 	if err != nil {
 		logrus.WithError(err).Errorln("获取救济金配置失败")
